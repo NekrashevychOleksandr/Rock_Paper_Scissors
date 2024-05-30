d0bc6a57cb8eec671abf6978d53277373711841e
Oleksandr Nekrashevych <shonnekrashevych@gmail.com>
2024-05-30 22:15:06 +0200

    added bot that can make random moves

---
+++ backend.py
@@ -0,0 +1,14 @@
+import random
+
+# Bot that plays rock, paper or scissors
+class RPS_bot:
+    def __init__(self):
+        pass
+    
+    # Makes a move randomly
+    def make_random_move(self):
+        return random.choice(['R', 'P', 'S'])
+
+
+
+    
