ffc9be2ddb846ee1bad7c1d2c3d62ecb16e38677
Oleksandr Nekrashevych <shonnekrashevych@gmail.com>
2024-06-01 18:49:43 +0200

    new

--- backend.py
+++ backend.py
@@ -4,16 +4,12 @@ import random
 class RPS_bot:
 
     def __init__(self):
-
         self.wins = 10
-        
-
-        return
     
     # Makes a move randomly
     def make_random_move(self):
-        wins = 5
-        print(self.wins)
-        print(wins)
         return random.choice(['R', 'P', 'S'])
+    
+
+
 
