0ff7db7360df6a8cee44d49fbc3a5a907ff02814
JaHeyl <Johard.Heyl@gmail.com>
2024-06-01 21:15:02 +0200

    Trying to put in image output

--- backend.py
+++ backend.py
@@ -15,15 +15,15 @@ class RPS_bot:
 
 
 
-
-
-class move_display_player_1:
+#Below is display for a player hand, that throws rock, paper, scissors
+    class move_display_player_1:
     def __init__(self, image_path):
-     self.image_path= 
+     self.image_path= image_path
      self.image= None
 
-    def load_image(self)
+    def load_image(self):
        if self.image:
           plt.imshow(self.image)
           plt.axis ('off')
           plt.show()
+          pass
